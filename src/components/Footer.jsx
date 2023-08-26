68c84951fd6139dccbfb1f5abae854e964029d02
guillermosifu <sifuentes1201@gmail.com>
2023-08-26 12:27:25 -0500

    carro2

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = ({ currentDate }) => {
+  return (
+    <footer className="footer">
+      <p>Todos los derechos reservados Guillermo Sifuentes  &copy; {currentDate}</p>
+    </footer>
+  );
+}
+
+export default Footer;
